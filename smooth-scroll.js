ba2c66dc41f2b2f09a4f289fb633b3b0f5cd95fa
warbodi4 <warbodi4@gmail.com>
2019-04-10 22:51:14 +0300

    Add js files(cart.js,index.js,product.js,smooth-scroll.js)

---
+++ smooth-scroll.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.smooth').on('click', function(event) {
+        const target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
